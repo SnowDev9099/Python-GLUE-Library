0c5a8729c1ee89a7473f19335f92ec24465631a7
KiwiOnGit <169009311+SnowDev9099@users.noreply.github.com>
2025-11-10 20:33:54 -0500

    Enhance GLUE class with color sensor and calculations and motor pair run and motor pair stop
    Added color sensor functionality and calculation methods and added a forever run forward and a way to halt forever running forward with motor pair.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from hub import port
-import runloop, motor, time
+import runloop, motor, time, color_sensor
 
 async def main():
     # Example code using the GLUE library.
@@ -8,6 +8,8 @@ async def main():
 
     motor.run_to_absolute_position(port.E, 36, 1000) 
     time.sleep_ms(100)
+
+
 
 
 
@@ -35,13 +37,13 @@ class GLUE:
 
     # motor pair is already a function but when you have your drivebase hooked up to it, it tends to "get a bit funky" this aims to solve that
     @staticmethod
-    async def MotorPairMoveForward(port_first=port.C, port_second=port.E, DurationInMS=1000, Velocity=200): # port.C and port.E are typically the ones used.
+    async def MotorPairMoveForward(port_first=port.C, port_second=port.E, DurationInMS=1000, Velocity=200): # port.C and port.E are typically the ones used
         motor.run_for_time(port_first, DurationInMS, Velocity)
         motor.run_for_time(port_second, DurationInMS, -Velocity)
 
 
     @staticmethod
-    async def MotorPairMoveBackward(port_first=port.C, port_second=port.E, DurationInMS=1000, Velocity=200): # port.C and port.E are typically the ones used.
+    async def MotorPairMoveBackward(port_first=port.C, port_second=port.E, DurationInMS=1000, Velocity=200): # port.C and port.E are typically the ones used
         motor.run_for_time(port_first, DurationInMS, -Velocity)
         motor.run_for_time(port_second, DurationInMS, Velocity)
 
@@ -56,6 +58,42 @@ class GLUE:
     async def TurnRight(velocity=-500):
         time.sleep_ms(10)
         motor.run_for_time(port.E, 500, -velocity)
+
+    @staticmethod
+    def GetCurrentColor(port_first=port.A):
+        return color_sensor.color(port_first)
+
+    @staticmethod
+    async def Calculate(First, Second, Add, Subtract, Multiply, Divide): # good for things you'll need to calcute either being async or easier with a method. you could calculate distance posibly with speed and distance sensor?
+        if Add:
+            return First + Second
+        elif Subtract:
+            return First - Second
+        elif Multiply:
+            return First * Second
+        elif Divide:
+            if Second == 0:
+                print("Divide by zero error!")
+            else:
+                return First / Second
+
+    @staticmethod
+    def MotorPairStop():
+        global MotorStop
+        MotorStop = True
+
+
+    @staticmethod
+    def MotorPairRun(port_first=port.C, port_second=port.E, Velocity=200):
+        global MotorStop
+        MotorStop = False
+        while not MotorStop:
+            motor.run(port_first, Velocity)
+            motor.run(port_second, -Velocity)
+            time.sleep_ms(50)# prevents code from stopping and overloading the CPU on the Lego Spike robot
+        motor.stop(port_first)
+        motor.stop(port_second)
+
 
 
 runloop.run(main())
